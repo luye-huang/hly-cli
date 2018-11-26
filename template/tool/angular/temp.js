6e7d826165a7ac58242c7eaca2efd628a3079473
huangluye <huangluye@qiyi.com>
2018-11-26 13:21:29 +0800

    adding more tpl

---
+++ template/tool/angular/temp.js
@@ -0,0 +1,3 @@
+import <%= name %>Component from './<%= name %>.component';
+export default angular.module('<%= name %>', [])
+.component('<%= name %>', <%= name %>Component);
---
+++ template/tool/react/temp.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+export default class <%= name %> extends Component {
+    //状态初始化 -- input输入框 和 下拉列表dropdown
+    constructor(props) {
+        super();
+        this.state = {
+        };
+    }
+
+    componentWillMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+
+        );
+    }
+
+    componentDidMount() {
+
+    }
+
+}
